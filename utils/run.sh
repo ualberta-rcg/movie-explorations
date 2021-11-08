35caca916fad13ff34b5ad66d213df671538b99b
Chris Want <cjwant@ualberta.ca>
2021-11-08 06:22:35 -0800

    Work on run.sh and showings demo.

--- utils/run.sh
+++ utils/run.sh
@@ -1,2 +1,17 @@
-./prepare-data.sh
-./launch-notebook.sh
+#!/bin/bash
+
+set -e
+
+[ -n "$SLURM_TMPDIR" ] || echo "No temp dir"
+[ -n "$SLURM_TMPDIR" ] || exit
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DATA_DIR="${MOVIE_DATA:-$SCRIPT_DIR/..}"
+
+cp $DATA_DIR/movies_json.tar.gz $SLURM_TMPDIR
+cd $SLURM_TMPDIR
+
+echo -e "bash $SCRIPT_DIR/make-and-activate-venv.sh
+         tar xf movies_json.tar.gz" | parallel
+
+bash $SCRIPT_DIR/launch-notebook.sh
